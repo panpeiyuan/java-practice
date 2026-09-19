f156e10d2ce96e7167a6a509dd11e37698c6080d
panpeiyuan <327372935+panpeiyuan@users.noreply.github.com>
2026-09-19 21:09:02 +0800

    MethodTest

--- src/chap6/MethodTest5.java
+++ src/chap6/MethodTest5.java
@@ -1,4 +1,26 @@
 package chap6;
 
 public class MethodTest5 {
+    public static void main(String[] args){
+        int[] arr={1,2,3,4,5,6,7,8,9};
+        System.out.print("[");
+        int[] arr1=copyOfRange(arr,3,7);
+        for (int i = 0; i < arr1.length; i++) {
+            if(i==arr1.length-1){
+                System.out.print(arr1[i]);
+            }else{
+                System.out.print(arr1[i]+",");
+            }
+        }
+        System.out.print("]");
+    }
+    public static int[] copyOfRange(int[] arr,int from,int to){
+        int[] arr_new=new int[to-from];
+        int j=0;
+        for(int i=from;i<to;i++){
+            arr_new[j]=arr[i];
+            j++;
+        }
+        return arr_new;
+    }
 }
